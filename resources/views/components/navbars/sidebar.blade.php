d245d495af1d118472a25bece5ab55ac23c2e460
Christhian Alexander <christhianalexander3@gmail.com>
2025-12-25 22:08:49 +0700

    update 6

--- resources/views/components/navbars/sidebar.blade.php
+++ resources/views/components/navbars/sidebar.blade.php
@@ -1,30 +1,62 @@
-<ul class="navbar-nav">
-    <li class="nav-item">
-        <a class="nav-link text-white {{ Route::currentRouteName() == 'dashboard' ? ' active bg-gradient-primary' : '' }}" href="{{ route('dashboard') }}">
-            <i class="material-icons opacity-10">dashboard</i> <span class="ms-1">Dashboard</span>
+<aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 bg-gradient-dark" id="sidenav-main">
+    <div class="sidenav-header">
+        <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+        <a class="navbar-brand m-0" href="{{ route('dashboard') }}">
+            <img src="{{ asset('assets') }}/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
+            <span class="ms-1 font-weight-bold text-white">Nama Website Anda</span>
         </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link text-white {{ Route::currentRouteName() == 'inventory' ? ' active bg-gradient-primary' : '' }}" href="{{ route('inventory') }}">
-            <i class="material-icons opacity-10">inventory_2</i> <span class="ms-1">Katalog</span>
-        </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link text-white {{ Route::currentRouteName() == 'customers' ? ' active bg-gradient-primary' : '' }}" href="{{ route('customers') }}">
-            <i class="material-icons opacity-10">groups</i> <span class="ms-1">Pelanggan</span>
-        </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link text-white {{ Route::currentRouteName() == 'reports' ? ' active bg-gradient-primary' : '' }}" href="{{ route('reports') }}">
-            <i class="material-icons opacity-10">assessment</i> <span class="ms-1">Laporan</span>
-        </a>
-    </li>
+    </div>
+    <hr class="horizontal light mt-0 mb-2">
+    <div class="collapse navbar-collapse w-auto max-height-vh-100" id="sidenav-collapse-main">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link text-white {{ Route::currentRouteName() == 'dashboard' ? ' active bg-gradient-primary' : '' }}" href="{{ route('dashboard') }}">
+                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="material-icons opacity-10">dashboard</i>
+                    </div>
+                    <span class="nav-link-text ms-1">Dashboard</span>
+                </a>
+            </li>
 
-    <li class="nav-item mt-3"><h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Point of Sale</h6></li>
-    
-    <li class="nav-item">
-        <a class="nav-link text-white {{ Route::currentRouteName() == 'pos' ? ' active bg-gradient-success' : '' }}" href="{{ route('pos') }}">
-            <i class="material-icons opacity-10">shopping_cart</i> <span class="ms-1">Buka Kasir</span>
-        </a>
-    </li>
-</ul>
+            <li class="nav-item">
+                <a class="nav-link text-white {{ Route::currentRouteName() == 'inventory' ? ' active bg-gradient-primary' : '' }}" href="{{ route('inventory') }}">
+                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="material-icons opacity-10">inventory_2</i>
+                    </div>
+                    <span class="nav-link-text ms-1">Katalog Produk</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link text-white {{ Route::currentRouteName() == 'customers' ? ' active bg-gradient-primary' : '' }}" href="{{ route('customers') }}">
+                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="material-icons opacity-10">groups</i>
+                    </div>
+                    <span class="nav-link-text ms-1">Pelanggan</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link text-white {{ Route::currentRouteName() == 'reports' ? ' active bg-gradient-primary' : '' }}" href="{{ route('reports') }}">
+                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="material-icons opacity-10">assessment</i>
+                    </div>
+                    <span class="nav-link-text ms-1">Laporan Penjualan</span>
+                </a>
+            </li>
+
+            <li class="nav-item mt-3">
+                <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Transaksi</h6>
+            </li>
+            
+            <li class="nav-item">
+                <a class="nav-link text-white {{ Route::currentRouteName() == 'pos' ? ' active bg-gradient-success' : '' }}" href="{{ route('pos') }}">
+                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="material-icons opacity-10">shopping_cart</i>
+                    </div>
+                    <span class="nav-link-text ms-1">Buka Kasir (POS)</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</aside>
